38b67649ae4cbada27e64a47cadf82ccc33b3e0f
unknown <hong7498kim@naver.com>
2025-07-14 16:11:25 +0900

    수정

--- websearch_based_RAG/chatbot_main.py
+++ websearch_based_RAG/chatbot_main.py
@@ -3,8 +3,9 @@ import streamlit as st
 from rag_chatbot import rag_chatbot
 
 def main_page():
-    st.title('교육과정 이론 Chat bot')
-    st.text('chat-bot을 활용하여 교육과정 이론을 공부하세요.')
+    st.title('면접 준비 보조 에이전트')
+    st.text('면접 절차 & 기출 질문을 준비하세요.')
+    st.text('취업 성공을 기원합니다!')
 
 page_names_to_funcs = {"Main Page": main_page, 'chat-bot': rag_chatbot}
 selected_page = st.sidebar.selectbox('Select a page', page_names_to_funcs)
