cb672d51040763924d33616df432d007d1ff1171
unknown <hong7498kim@naver.com>
2025-07-16 15:24:18 +0900

    main 페이지 이미지 추가

--- websearch_based_RAG_FAISS/chatbot_main.py
+++ websearch_based_RAG_FAISS/chatbot_main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 from rag_chatbot import rag_chatbot
 
@@ -6,6 +7,9 @@ def main_page():
     st.title('면접 준비 보조 에이전트')
     st.text('면접 절차 & 기출 질문을 준비하세요.')
     st.text('취업 성공을 기원합니다!')
+    
+    image = Image.open('image.png')
+    st.image(image, caption="취업 성공을 기원합니다!")
 
 page_names_to_funcs = {"Main Page": main_page, 'chat-bot': rag_chatbot}
 selected_page = st.sidebar.selectbox('Select a page', page_names_to_funcs)
